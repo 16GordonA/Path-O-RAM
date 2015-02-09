075cb5a77f08f2e65ffcd918540a8e7e4c3698c4
16GordonA <16gordona@my.weston.org>
2015-02-09 14:15:33 -0500

    fixed some minor formatting stuff

--- Tree.py
+++ Tree.py
@@ -1,29 +1,34 @@
 class Tree:
-  def __init__ (self, layers, bucket_size):
-    self.l = layers
-    self.z = bucket_size
+    def __init__ (self, layers, bucket_size):
+        self.l = layers
+        self.z = bucket_size
+        self.blocks = [for i in range(self.z)] #I think this is the right way to do this
     
-    if(self.l > 1):
-      self.tL = Tree(layers-1, bucket_size) #use recursive tree (left branch)
-      self.tR = Tree(layers-1, bucket_size) #use recursive tree (right branch)
-    else:
-      self.leaf = Leaf(bucket_size)
+        if(self.l > 2):
+            self.node1 = Tree(layers-1, bucket_size) #use recursive tree (left branch)
+            self.node2 = Tree(layers-1, bucket_size) #use recursive tree (right branch)
+        else:
+            self.node1 = Leaf(bucket_size)
+            self.node2 = Leaf(bucket_size)
     
-  def pushDown(self):
-    #do what it takes to push everything down...
+    def pushDown(self):
+        #do what it takes to push everything down...
     
-  def getBlock(self, path):
-    #some sort of recursive thing
+    def readBlock(self, path):
+        #some sort of recursive thing
+        
+    def writeBlock(self, path, data):
+        #write to the new block, then call
   
-  def setBlock(self, new_path, old_path):
-    #some sort of recursive thing involving getBlock(old_path)
+    def moveBlock(self, new_path, old_path):
+        #some sort of recursive thing involving getBlock(old_path)
     
-  def getZ(self):
-    return self.z
+    def getZ(self):
+        return self.z
     
-  def setZ(self, new_z):
-    self.z = new_z
-    return self.z
+    def setZ(self, new_z):
+        self.z = new_z
+        return self.z
   
-  def getL(self):
-    return self.l
+    def getL(self):
+        return self.l
